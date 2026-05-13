f92d7732a6619cdd8d4633866d3a6352324647ec
Abhishek SS <Sahukarabhi.10@gmail.com>
2026-05-13 12:22:24 +0200

    Readme and code fix

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import os
 
-<<<<<<< HEAD
 os.makedirs("app/static/assets", exist_ok=True)
 os.makedirs("app/templates", exist_ok=True)
 app = FastAPI()
@@ -19,12 +18,10 @@ app.mount("/assets", StaticFiles(directory="app/static/assets"), name="assets")
 
 
 
-=======
 load_dotenv()
 
 app = FastAPI(title="DreamCatcher API")
 
->>>>>>> 142df6a (Readme and code fix)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -55,16 +52,4 @@ async def analyze_dream(data: DreamInput):
         interpretation = interpret_dream(data.dream)
         return {"interpretation": interpretation}
     except Exception as e:
-<<<<<<< HEAD
-        import traceback
-        print("🔥 Error during interpretation:", str(e))
-        traceback.print_exc()
-        raise e
-
-
-@app.get("/")
-def serve_frontend():
-    return FileResponse("app/templates/index.html")
-=======
         raise HTTPException(status_code=500, detail=str(e))
->>>>>>> 142df6a (Readme and code fix)
